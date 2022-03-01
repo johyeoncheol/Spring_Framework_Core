622e7e4d9da52be040b5b135ae2201bcb1a5972f
johyeoncheol <qweas46@naver.com>
2022-03-01 20:43:58 +0900

    (feat:hyeoncheol) 로그 파라미터로 적용

---
+++ 김영한 스프링 핵심원리 고급편/code/src/main/java/hello/advanced/app/v2/OrderServiceV2.java
@@ -0,0 +1,32 @@
+package hello.advanced.app.v2;
+
+import hello.advanced.trace.TraceId;
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.hellotrace.HelloTraceV2;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV2 {
+
+    private final OrderRepositoryV2 orderRepository;
+    private final HelloTraceV2 trace;
+
+    //RequiredArgsConstructor을 대신함
+//    public OrderService(OrderRepositoryV0 orderRepositoryV0) {
+//        this.orderRepositoryV0 = orderRepositoryV0;
+//    }
+
+    public void orderItem(TraceId traceId, String itemId) {
+        TraceStatus status = null;
+        try {
+            status = trace.beginSync(traceId,"OrderServiceV1.request()");
+            orderRepository.save(status.getTraceId(),itemId);
+            trace.end(status);
+        }catch (Exception e){
+            trace.exception(status,e);
+            throw e;
+        }
+    }
+}
