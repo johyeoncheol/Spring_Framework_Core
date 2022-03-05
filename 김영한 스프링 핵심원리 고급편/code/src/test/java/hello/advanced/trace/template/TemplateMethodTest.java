139cb389c7669442b372c1ad278ea3ffcd008d36
johyeoncheol <qweas46@naver.com>
2022-03-05 19:21:11 +0900

    (feat:hyeoncheol) 템플릿 메서드 패턴 예제 1

---
+++ 김영한 스프링 핵심원리 고급편/code/src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
@@ -0,0 +1,34 @@
+package hello.advanced.trace.template;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class TemplateMethodTest {
+
+    @Test
+    void templateMethodV0(){
+        logical1();
+        logical2();
+    }
+
+    private void logical1(){
+        long startTime = System.currentTimeMillis();
+        //비지니스 로직 실행
+        log.info("비지니스 로직1 실행");
+        //비지니스 로직 종료
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime={}",resultTime);
+    }
+
+    private void logical2(){
+        long startTime = System.currentTimeMillis();
+        //비지니스 로직 실행
+        log.info("비지니스 로직2 실행");
+        //비지니스 로직 종료
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime={}",resultTime);
+    }
+}
