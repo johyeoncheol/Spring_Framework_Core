4c2c5d5f536d0ad04cd8308bf660004c91f77db6
johyeoncheol <qweas46@naver.com>
2022-03-06 14:12:58 +0900

    (feat:hyeoncheol) 로그 추적기 템플릿 패턴 적용

---
+++ 김영한 스프링 핵심원리 고급편/code/src/main/java/hello/advanced/app/v4/OrderServiceV4.java
@@ -0,0 +1,31 @@
+package hello.advanced.app.v4;
+
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.logtrace.LogTrace;
+import hello.advanced.trace.template.AbstractTemplate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV4 {
+
+    private final OrderRepositoryV4 orderRepository;
+    private final LogTrace trace;
+
+    //RequiredArgsConstructor을 대신함
+//    public OrderService(OrderRepositoryV0 orderRepositoryV0) {
+//        this.orderRepositoryV0 = orderRepositoryV0;
+//    }
+
+    public void orderItem(String itemId) {
+        AbstractTemplate<Void> template = new AbstractTemplate<>(trace) {
+            @Override
+            protected Void call() {
+                orderRepository.save(itemId);
+                return null;
+            }
+        };
+       template.execute("OrderServiceV4.orderItem()");
+    }
+}
