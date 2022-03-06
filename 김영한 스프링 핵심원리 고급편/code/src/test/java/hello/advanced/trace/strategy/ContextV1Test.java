ae44ea1c8c15a383c87956ec4482903d2b446b69
johyeoncheol <qweas46@naver.com>
2022-03-06 14:27:44 +0900

    (feat:hyeoncheol) 전략 패턴 예제

---
+++ 김영한 스프링 핵심원리 고급편/code/src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
@@ -0,0 +1,33 @@
+package hello.advanced.trace.strategy;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ContextV1Test {
+    @Test
+    void strategyV0(){
+        logical1();
+        logical2();
+    }
+
+    private void logical1(){
+        long startTime = System.currentTimeMillis();
+        //비지니스 로직 실행
+        log.info("비지니스 로직1 실행");
+        //비지니스 로직 종료
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime={}",resultTime);
+    }
+
+    private void logical2(){
+        long startTime = System.currentTimeMillis();
+        //비지니스 로직 실행
+        log.info("비지니스 로직2 실행");
+        //비지니스 로직 종료
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime={}",resultTime);
+    }
+}
