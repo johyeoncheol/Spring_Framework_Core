f73c24bc401ec72722057de08781dc9549a25ac0
johyeoncheol <qweas46@naver.com>
2022-03-06 15:01:05 +0900

    (feat:hyeoncheol) 전략 패턴

--- 김영한 스프링 핵심원리 고급편/code/src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
+++ 김영한 스프링 핵심원리 고급편/code/src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
@@ -1,5 +1,8 @@
 package hello.advanced.trace.strategy;
 
+import hello.advanced.trace.strategy.code.strategy.ContextV1;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogic1;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogic2;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -29,5 +32,18 @@ public class ContextV1Test {
         long endTime = System.currentTimeMillis();
         long resultTime = endTime - startTime;
         log.info("resultTime={}",resultTime);
+    }
+
+    /**
+     * 전략 패턴 사
+     */
+    @Test
+    void strategyV1(){
+        StrategyLogic1 strategyLogic1 = new StrategyLogic1();
+        ContextV1 context1 = new ContextV1(strategyLogic1);
+        context1.execute();
+        StrategyLogic2 strategyLogic2 = new StrategyLogic2();
+        ContextV1 context2 = new ContextV1(strategyLogic1);
+        context2.execute();
     }
 }
