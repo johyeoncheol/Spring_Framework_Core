d233e07f55ff6e5bffe517be5d0187947884abf1
johyeoncheol <qweas46@naver.com>
2022-03-10 12:01:28 +0900

    (feat:hyeoncheol) fix: v2 controller GetMapping 추가

--- 김영한 스프링 핵심원리 고급편/proxy-code/src/main/java/hello/proxy/app/v2/OrderControllerV2.java
+++ 김영한 스프링 핵심원리 고급편/proxy-code/src/main/java/hello/proxy/app/v2/OrderControllerV2.java
@@ -1,6 +1,7 @@
 package hello.proxy.app.v2;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -15,11 +16,13 @@ public class OrderControllerV2 {
         this.orderService = orderService;
     }
 
+    @GetMapping("/v2/request")
     public String request(String itemId) {
         orderService.orderItem(itemId);
         return "ok";
     }
 
+    @GetMapping("/v2/no-log")
     public String noLog() {
         return "ok";
     }
