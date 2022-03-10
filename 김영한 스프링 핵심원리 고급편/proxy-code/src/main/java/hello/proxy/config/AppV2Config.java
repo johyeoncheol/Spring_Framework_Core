a4254f37f301b15a373181feaea19517b7684262
johyeoncheol <qweas46@naver.com>
2022-03-10 12:01:04 +0900

    (feat:hyeoncheol) v2 Config 설정

---
+++ 김영한 스프링 핵심원리 고급편/proxy-code/src/main/java/hello/proxy/config/AppV2Config.java
@@ -0,0 +1,24 @@
+package hello.proxy.config;
+
+import hello.proxy.app.v2.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppV2Config {
+
+    @Bean
+    public OrderControllerV2 orderControllerV2(){
+        return new OrderControllerV2(orderServiceV2());
+    }
+
+    @Bean
+    public OrderServiceV2 orderServiceV2() {
+        return new OrderServiceV2(orderRepositoryV2());
+    }
+
+    @Bean
+    public OrderRepositoryV2 orderRepositoryV2() {
+        return new OrderRepositoryV2();
+    }
+}
