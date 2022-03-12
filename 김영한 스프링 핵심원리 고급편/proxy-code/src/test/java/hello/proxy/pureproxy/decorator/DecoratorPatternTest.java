bf891ccf44ce7185262b062a1ad9c108dff174b2
johyeoncheol <qweas46@naver.com>
2022-03-12 22:55:38 +0900

    (feat:hyeoncheol) 데코레이터 패턴 예제

--- 김영한 스프링 핵심원리 고급편/proxy-code/src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
+++ 김영한 스프링 핵심원리 고급편/proxy-code/src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
@@ -1,9 +1,6 @@
 package hello.proxy.pureproxy.decorator;
 
-import hello.proxy.pureproxy.decorator.code.Component;
-import hello.proxy.pureproxy.decorator.code.DecoratorPatternClient;
-import hello.proxy.pureproxy.decorator.code.MessageDecorator;
-import hello.proxy.pureproxy.decorator.code.RealComponent;
+import hello.proxy.pureproxy.decorator.code.*;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -22,6 +19,15 @@ public class DecoratorPatternTest {
         Component realComponent = new RealComponent();
         Component messageDecorator = new MessageDecorator(realComponent);
         DecoratorPatternClient client = new DecoratorPatternClient(messageDecorator);
+        client.execute();
+    }
+
+    @Test
+    void decorator2(){
+        Component realComponent = new RealComponent();
+        Component messageDecorator = new MessageDecorator(realComponent);
+        Component timeDecorator = new TimeDecorator(messageDecorator);
+        DecoratorPatternClient client = new DecoratorPatternClient(timeDecorator);
         client.execute();
     }
 }
