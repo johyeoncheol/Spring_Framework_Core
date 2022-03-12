dd9d56ddd693cf817e995cc399842499d926629b
johyeoncheol <qweas46@naver.com>
2022-03-12 22:37:42 +0900

    (feat:hyeoncheol) Message 데코레이터 추가

--- 김영한 스프링 핵심원리 고급편/proxy-code/src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
+++ 김영한 스프링 핵심원리 고급편/proxy-code/src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
@@ -2,6 +2,7 @@ package hello.proxy.pureproxy.decorator;
 
 import hello.proxy.pureproxy.decorator.code.Component;
 import hello.proxy.pureproxy.decorator.code.DecoratorPatternClient;
+import hello.proxy.pureproxy.decorator.code.MessageDecorator;
 import hello.proxy.pureproxy.decorator.code.RealComponent;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
@@ -13,6 +14,14 @@ public class DecoratorPatternTest {
     void noDecorator(){
         Component realComponent = new RealComponent();
         DecoratorPatternClient client = new DecoratorPatternClient(realComponent);
+        client.execute();
+    }
+
+    @Test
+    void decorator1(){
+        Component realComponent = new RealComponent();
+        Component messageDecorator = new MessageDecorator(realComponent);
+        DecoratorPatternClient client = new DecoratorPatternClient(messageDecorator);
         client.execute();
     }
 }
