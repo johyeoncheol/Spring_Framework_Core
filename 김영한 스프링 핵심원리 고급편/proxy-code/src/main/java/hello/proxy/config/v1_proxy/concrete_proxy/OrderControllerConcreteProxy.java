e6312db55893249bab41f4f8a2fb1ba09388c4dc
johyeoncheol <qweas46@naver.com>
2022-03-14 21:18:22 +0900

    (feat:hyeoncheol) 구체 클래스 적용

---
+++ 김영한 스프링 핵심원리 고급편/proxy-code/src/main/java/hello/proxy/config/v1_proxy/concrete_proxy/OrderControllerConcreteProxy.java
@@ -0,0 +1,17 @@
+package hello.proxy.config.v1_proxy.concrete_proxy;
+
+import hello.proxy.app.v2.OrderControllerV2;
+import hello.proxy.app.v2.OrderServiceV2;
+import hello.proxy.trace.logtrace.LogTrace;
+
+public class OrderControllerConcreteProxy extends OrderControllerV2 {
+
+    private final OrderControllerV2 target;
+    private final LogTrace logTrace;
+
+    public OrderControllerConcreteProxy(OrderControllerV2 target, LogTrace logTrace) {
+        super(null);
+        this.target = target;
+        this.logTrace = logTrace;
+    }
+}
