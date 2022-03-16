55dcebeeeef7d9bc64a4ea4f7f5de1c91c3fa421
johyeoncheol <qweas46@naver.com>
2022-03-16 12:22:29 +0900

    (feat:hyeoncheol) 동적 프록시 적용 1

--- 김영한 스프링 핵심원리 고급편/proxy-code/src/main/java/hello/proxy/config/v1_proxy/concrete_proxy/OrderControllerConcreteProxy.java
+++ 김영한 스프링 핵심원리 고급편/proxy-code/src/main/java/hello/proxy/config/v1_proxy/concrete_proxy/OrderControllerConcreteProxy.java
@@ -2,6 +2,7 @@ package hello.proxy.config.v1_proxy.concrete_proxy;
 
 import hello.proxy.app.v2.OrderControllerV2;
 import hello.proxy.app.v2.OrderServiceV2;
+import hello.proxy.trace.TraceStatus;
 import hello.proxy.trace.logtrace.LogTrace;
 
 public class OrderControllerConcreteProxy extends OrderControllerV2 {
@@ -13,5 +14,19 @@ public class OrderControllerConcreteProxy extends OrderControllerV2 {
         super(null);
         this.target = target;
         this.logTrace = logTrace;
+    }
+
+    @Override
+    public String request(String itemId) {
+        TraceStatus status=null;
+        try {
+            status = logTrace.begin("OrderController.orderItem()");
+            String result = target.request(itemId);
+            logTrace.end(status);
+            return result;
+        }catch (Exception e){
+            logTrace.exception(status,e);
+            throw e;
+        }
     }
 }
