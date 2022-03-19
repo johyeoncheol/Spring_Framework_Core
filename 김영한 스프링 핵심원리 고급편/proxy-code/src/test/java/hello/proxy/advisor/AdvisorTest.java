b81b978980f93896474673f3526b040db586badf
johyeoncheol <qweas46@naver.com>
2022-03-19 16:38:36 +0900

    (feat:hyeoncheol) 어드바이저 예제

---
+++ 김영한 스프링 핵심원리 고급편/proxy-code/src/test/java/hello/proxy/advisor/AdvisorTest.java
@@ -0,0 +1,24 @@
+package hello.proxy.advisor;
+
+import hello.proxy.common.Service.ServiceImpl;
+import hello.proxy.common.Service.ServiceInterface;
+import hello.proxy.common.advice.TimeAdvice;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.Pointcut;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+
+public class AdvisorTest {
+
+    @Test
+    void advisorTest1(){
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(Pointcut.TRUE,new TimeAdvice());
+        proxyFactory.addAdvisor(advisor);
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+
+        proxy.save();
+        proxy.find();
+    }
+}
