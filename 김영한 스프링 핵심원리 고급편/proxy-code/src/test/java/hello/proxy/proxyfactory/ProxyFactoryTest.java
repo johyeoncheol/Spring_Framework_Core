4ad60339be23703a4b517411ced7eebb54113ca4
johyeoncheol <qweas46@naver.com>
2022-03-19 14:37:52 +0900

    (feat:hyeoncheol) 프록시 팩토리 예제 2

--- 김영한 스프링 핵심원리 고급편/proxy-code/src/test/java/hello/proxy/proxyfactory/ProxyFactoryTest.java
+++ 김영한 스프링 핵심원리 고급편/proxy-code/src/test/java/hello/proxy/proxyfactory/ProxyFactoryTest.java
@@ -1,5 +1,6 @@
 package hello.proxy.proxyfactory;
 
+import hello.proxy.common.Service.ConcreteService;
 import hello.proxy.common.Service.ServiceImpl;
 import hello.proxy.common.Service.ServiceInterface;
 import hello.proxy.common.advice.TimeAdvice;
@@ -30,5 +31,40 @@ public class ProxyFactoryTest {
         assertThat(AopUtils.isAopProxy(proxy)).isTrue();
         assertThat(AopUtils.isJdkDynamicProxy(proxy)).isTrue();
         assertThat(AopUtils.isCglibProxy(proxy)).isFalse();
+    }
+
+    @Test
+    @DisplayName("구체 클래스만 있으면 CGLIB 사용")
+    void concreteProxy(){
+        ConcreteService target = new ConcreteService();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.addAdvice(new TimeAdvice());
+        ConcreteService proxy = (ConcreteService) proxyFactory.getProxy();
+        log.info("targetClass={}",target.getClass());
+        log.info("proxyClass={}",proxy.getClass());
+
+        proxy.call();
+
+        assertThat(AopUtils.isAopProxy(proxy)).isTrue();
+        assertThat(AopUtils.isJdkDynamicProxy(proxy)).isFalse();
+        assertThat(AopUtils.isCglibProxy(proxy)).isTrue();
+    }
+
+    @Test
+    @DisplayName("ProxyTargetClass 옵션을 사용하면 인터페이스가 있어도 CGLIB를 사용하고, 클래스 기반 프록시 사용")
+    void proxyTargetClass(){
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.setProxyTargetClass(true);
+        proxyFactory.addAdvice(new TimeAdvice());
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+        log.info("targetClass={}",target.getClass());
+        log.info("proxyClass={}",proxy.getClass());
+
+        proxy.save();
+
+        assertThat(AopUtils.isAopProxy(proxy)).isTrue();
+        assertThat(AopUtils.isJdkDynamicProxy(proxy)).isFalse();
+        assertThat(AopUtils.isCglibProxy(proxy)).isTrue();
     }
 }
