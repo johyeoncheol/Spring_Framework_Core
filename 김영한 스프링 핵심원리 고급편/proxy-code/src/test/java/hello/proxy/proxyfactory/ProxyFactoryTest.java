0a0744e569c4c2c156a37e4c9529bc4961e06d89
johyeoncheol <qweas46@naver.com>
2022-03-19 12:25:10 +0900

    (feat:hyeoncheol) 프록시 팩토리 예제 1

---
+++ 김영한 스프링 핵심원리 고급편/proxy-code/src/test/java/hello/proxy/proxyfactory/ProxyFactoryTest.java
@@ -0,0 +1,34 @@
+package hello.proxy.proxyfactory;
+
+import hello.proxy.common.Service.ServiceImpl;
+import hello.proxy.common.Service.ServiceInterface;
+import hello.proxy.common.advice.TimeAdvice;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.aop.support.AopUtils;
+
+import static org.assertj.core.api.Assertions.*;
+
+@Slf4j
+public class ProxyFactoryTest {
+
+    @Test
+    @DisplayName("인터페이스가 있으면 JDK 동적 프록시 사용")
+    void interfaceProxy(){
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.addAdvice(new TimeAdvice());
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+        log.info("targetClass={}",target.getClass());
+        log.info("proxyClass={}",proxy.getClass());
+
+        proxy.save();
+
+        assertThat(AopUtils.isAopProxy(proxy)).isTrue();
+        assertThat(AopUtils.isJdkDynamicProxy(proxy)).isTrue();
+        assertThat(AopUtils.isCglibProxy(proxy)).isFalse();
+    }
+}
