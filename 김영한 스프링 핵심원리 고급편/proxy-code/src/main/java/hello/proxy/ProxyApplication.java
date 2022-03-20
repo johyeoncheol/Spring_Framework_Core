55f224d675705612c200a72bbffe39d96a9d3ef9
johyeoncheol <qweas46@naver.com>
2022-03-20 16:40:15 +0900

    (feat:hyeoncheol) 프록시 팩토리 V2 적용

--- 김영한 스프링 핵심원리 고급편/proxy-code/src/main/java/hello/proxy/ProxyApplication.java
+++ 김영한 스프링 핵심원리 고급편/proxy-code/src/main/java/hello/proxy/ProxyApplication.java
@@ -7,6 +7,7 @@ import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
 import hello.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
 import hello.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
 import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV1;
+import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV2;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.aop.framework.ProxyFactory;
@@ -20,7 +21,8 @@ import org.springframework.context.annotation.Import;
 //@Import({ConcreteProxyConfig.class})
 //@Import({DynamicProxyBasicConfig.class})
 //@Import({DynamicProxyFilterConfig.class})
-@Import(ProxyFactoryConfigV1.class)
+//@Import(ProxyFactoryConfigV1.class)
+@Import(ProxyFactoryConfigV2.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
