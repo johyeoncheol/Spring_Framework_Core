37e25f7dd4360a2db65c0f6b61f6ab70fe380b9c
johyeoncheol <qweas46@naver.com>
2022-03-26 12:57:21 +0900

    (feat:hyeoncheol) @Aspect 프록시

--- 김영한 스프링 핵심원리 고급편/proxy-code/src/main/java/hello/proxy/ProxyApplication.java
+++ 김영한 스프링 핵심원리 고급편/proxy-code/src/main/java/hello/proxy/ProxyApplication.java
@@ -9,6 +9,7 @@ import hello.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
 import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV1;
 import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV2;
 import hello.proxy.config.v4_postprocessor.postprocessor.BeanPostProcessorConfig;
+import hello.proxy.config.v6_aop.AopConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.aop.framework.ProxyFactory;
@@ -24,7 +25,8 @@ import org.springframework.context.annotation.Import;
 //@Import({DynamicProxyFilterConfig.class})
 //@Import(ProxyFactoryConfigV1.class)
 //@Import(ProxyFactoryConfigV2.class)
-@Import(BeanPostProcessorConfig.class)
+//@Import(BeanPostProcessorConfig.class)
+@Import(AopConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
