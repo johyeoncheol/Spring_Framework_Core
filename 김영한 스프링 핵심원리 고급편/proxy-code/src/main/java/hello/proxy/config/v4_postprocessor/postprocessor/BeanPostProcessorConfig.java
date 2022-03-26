98ecbf5dcc99675156c2fbe6277a0116655d261f
johyeoncheol <qweas46@naver.com>
2022-03-26 10:46:26 +0900

    (feat:hyeoncheol) 빈 후기 처리 마무리

--- 김영한 스프링 핵심원리 고급편/proxy-code/src/main/java/hello/proxy/config/v4_postprocessor/postprocessor/BeanPostProcessorConfig.java
+++ 김영한 스프링 핵심원리 고급편/proxy-code/src/main/java/hello/proxy/config/v4_postprocessor/postprocessor/BeanPostProcessorConfig.java
@@ -2,9 +2,12 @@ package hello.proxy.config.v4_postprocessor.postprocessor;
 
 import hello.proxy.config.AppV1Config;
 import hello.proxy.config.AppV2Config;
+import hello.proxy.config.v3_proxyfactory.advice.LogTraceAdvice;
 import hello.proxy.trace.logtrace.LogTrace;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.aop.Advisor;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.NameMatchMethodPointcut;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -20,6 +23,13 @@ public class BeanPostProcessorConfig {
     }
 
     private Advisor getAdvisor(LogTrace logTrace) {
-        return null;
+        //pointcut
+        NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
+        pointcut.setMappedNames("request*","order*","save*");
+
+        //adivce
+        LogTraceAdvice advice = new LogTraceAdvice(logTrace);
+
+        return new DefaultPointcutAdvisor(pointcut,advice);
     }
 }
