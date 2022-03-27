1a3ec27e9929f458075fb088e3d8d94f1014539b
johyeoncheol <qweas46@naver.com>
2022-03-27 15:58:46 +0900

    (feat:hyeoncheol) 포인트 컷 참조 구현

--- 김영한 스프링 핵심원리 고급편/aop-code/src/test/java/hello/aop/AopTest.java
+++ 김영한 스프링 핵심원리 고급편/aop-code/src/test/java/hello/aop/AopTest.java
@@ -5,6 +5,7 @@ import hello.aop.order.OrderService;
 import hello.aop.order.aop.AspectV1;
 import hello.aop.order.aop.AspectV2;
 import hello.aop.order.aop.AspectV3;
+import hello.aop.order.aop.AspectV4Pointcut;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -17,7 +18,8 @@ import org.springframework.context.annotation.Import;
 @SpringBootTest
 //@Import(AspectV1.class)
 //@Import(AspectV2.class)
-@Import(AspectV3.class)
+//@Import(AspectV3.class)
+@Import(AspectV4Pointcut.class)
 public class AopTest {
 
     @Autowired
