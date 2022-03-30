074b11867fd3c76ff4ae11845db56d433f9abdd9
johyeoncheol <qweas46@naver.com>
2022-03-30 17:30:49 +0900

    (feat:hyeoncheol) within 테스트 예제

---
+++ 김영한 스프링 핵심원리 고급편/aop-code/src/test/java/hello/aop/pointcut/WithinTest.java
@@ -0,0 +1,32 @@
+package hello.aop.pointcut;
+
+import hello.aop.member.MemberServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+
+import java.lang.reflect.Method;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class WithinTest {
+
+    AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+    Method helloMethod;
+
+    @BeforeEach
+    public void init() throws NoSuchMethodException {
+        helloMethod = MemberServiceImpl.class.getMethod("hello", String.class);
+    }
+
+    @Test
+    void withinExact(){
+        pointcut.setExpression("within(hello.aop.member.MemberServiceImpl)");
+        assertThat(pointcut.matches(helloMethod,MemberServiceImpl.class)).isTrue();
+    }
+
+    @Test
+    void withinStar(){
+        pointcut.setExpression("within(hello.aop.member.");
+    }
+}
