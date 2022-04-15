39ec8cbaf70949b519c03bbf74b83dc2c518d28d
johyeoncheol <qweas46@naver.com>
2022-04-15 16:53:45 +0900

    (feat:hyeoncheol) 내부 호출 Test V1

---
+++ 김영한 스프링 핵심원리 고급편/aop-code/src/test/java/hello/aop/internalcall/CallServiceV1Test.java
@@ -0,0 +1,26 @@
+package hello.aop.internalcall;
+
+import hello.aop.internalcall.aop.CallLogAspect;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(CallLogAspect.class)
+@SpringBootTest
+@Slf4j
+class CallServiceV1Test {
+
+    @Autowired CallServiceV1 callServiceV1;
+
+    @Test
+    void external() {
+        callServiceV1.external();
+    }
+
+    @Test
+    void internal() {
+        callServiceV1.internal();
+    }
+}
