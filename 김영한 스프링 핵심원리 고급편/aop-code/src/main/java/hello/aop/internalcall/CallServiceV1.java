28ec3f9f599293911c0201a1db406ff815fa8934
johyeoncheol <qweas46@naver.com>
2022-04-15 16:53:11 +0900

    (feat:hyeoncheol) 대안 1, 자기 자신 주입

---
+++ 김영한 스프링 핵심원리 고급편/aop-code/src/main/java/hello/aop/internalcall/CallServiceV1.java
@@ -0,0 +1,27 @@
+package hello.aop.internalcall;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class CallServiceV1 {
+
+    private CallServiceV1 callServiceV1;
+
+    @Autowired
+    public void setCallServiceV1(CallServiceV1 callServiceV1) {
+        log.info("CallServiceV1 setter={}",callServiceV1.getClass());
+        this.callServiceV1 = callServiceV1;
+    }
+
+    public void external(){
+        log.info("call external");
+        internal();
+    }
+
+    public void internal(){
+        log.info("call internal");
+    }
+}
